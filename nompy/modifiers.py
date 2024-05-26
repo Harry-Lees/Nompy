4736f130767928fa80ffadf37abcfeab2d10b7c9
Harry Lees <harry.lees@rlauto.com>
2024-05-26 05:42:24 +0100

    Use pre-3.12 generic syntax

--- nompy/modifiers.py
+++ nompy/modifiers.py
@@ -1,13 +1,18 @@
 from collections.abc import Callable
+from typing import TypeVar
 
-def apply[T](
+RT = TypeVar("RT")
+
+
+def apply(
     parser: Callable[[str], tuple[str, str]],
-    func: Callable[[str], T],
-) -> Callable[[str], tuple[T, str]]:
+    func: Callable[[str], RT],
+) -> Callable[[str], tuple[RT, str]]:
     """
     Apply a given function to the output of a parser.
     """
-    def inner(obj: str) -> tuple[T, str]:
+
+    def inner(obj: str) -> tuple[RT, str]:
         parse_result, remaining = parser(obj)
         return func(parse_result), remaining
 
